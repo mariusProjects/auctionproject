2aa806f154ba52ff4f23ba473b63d3b85b19deb9
marius <mariusvdascalu@gmail.com>
2020-09-23 12:07:28 +0300

    auction

---
+++ src/main/java/com/sda/auction/service/ItemService.java
@@ -0,0 +1,19 @@
+package com.sda.auction.service;
+
+import com.sda.auction.dto.ItemDto;
+import java.text.ParseException;
+import java.util.List;
+
+public interface ItemService {
+
+	ItemDto addItem(ItemDto itemDto, String ownerEmail) throws ParseException;
+
+
+	List<ItemDto> findAll();
+
+	ItemDto findByIdFor(Integer id, String userEmail);
+
+	List<ItemDto> findAllForBidding();
+
+	ItemDto findByIdForUser(Integer id, String email);
+}
